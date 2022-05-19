85ea578fe416e2db1493838fb003bed719700da1
Siqin Wang <siqinw97@gmail.com>
2022-05-20 00:13:41 +0800

    lab2 done

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -1,7 +1,9 @@
 package IntList;
 
-import static org.junit.Assert.*;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SquarePrimesTest {
 
@@ -15,6 +17,14 @@ public class SquarePrimesTest {
         IntList lst = IntList.of(14, 15, 16, 17, 18);
         boolean changed = IntListExercises.squarePrimes(lst);
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimes1() {
+        IntList lst = IntList.of(19, 20, 21, 22, 23);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("361 -> 20 -> 21 -> 22 -> 529", lst.toString());
         assertTrue(changed);
     }
 }
